cd9b4c3204ae792b4836b9b9638fa1e87c56855f
bilele123123 <tple06203@gmail.com>
2026-03-24 17:53:11 -0700

    feat: add new parameters to listing interface

--- src/interface/listings-interface.ts
+++ src/interface/listings-interface.ts
@@ -8,14 +8,22 @@ export interface IListing {
   created_at: string;
   description: string;
   host_id: string;
+  host_name?: string;
+  host_type?: string;
   id: string;
   images: string[];
   incentives: string[];
   is_active: boolean;
+  is_guest_favorite: boolean;
+  is_popular: boolean;
+  original_price?: number;
   parking_type: ParkingType;
   perks: string[];
   price_per_hour: number;
+  rating?: number;
+  review_count: number;
   structure_name?: string;
+  sub_heading: string[];
   title: string;
   updated_at: string;
 }
